a9946523f6602776fb6a04bf3271e03190da6b04
Lieene Guo <lieene@shaderealm.com>
2019-11-21 01:18:41 +0800

    test passde, added removeline addline updated Span api

--- __tests__/tf.test.ts
+++ __tests__/tf.test.ts
@@ -8,18 +8,22 @@
 import { Text } from "../src/index";
 import * as L from "@lieene/ts-utility";
 import * as path from "path";
+import Span = Text.Span;
+import Pos = Text.Pos;
+import Range = L.Range;
 test("text file test", () =>
 {
     let t = new Text(__filename, "Path").AsReadonly;
     console.log(t.slice(1, 7));
     t.BeginEdit(e =>
     {
-        e.insert(new Text.Pos(1,86), "Sneak");
-        e.insert(0, "Here comes the edit\n");
-        e.replace(new L.Range(3, 18), "the file is edited");
-        e.replace(new Text.Span([2, 4], [2, 10]), "ooooooo");
+        e.insert(new Pos(1, 86), "Sneak");
+        e.insert(172, "Snack");
+        e.insertLine([0, "Here comes the edit"], [8, "------code coming------"]);
+        e.replace(new Range(3, 18), "the file is edited");
+        e.replace(new Span([2, 4], [2, 10]), "ooooooo");
         e.remove(e.text.lineRange(7, "include line end")!);
         e.removeLine(10);
     }).then(t => console.log(t.source));
-    console.log(t.slice(new L.Range(3, 16)));
+    console.log(t.slice(new Range(3, 16)));
 });
